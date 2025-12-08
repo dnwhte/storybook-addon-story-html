7eda146549138e4029c9b86c76ad257baa9d2bf3
Dan White <dan.white1@wsu.edu>
2025-12-08 13:49:48 -0800

    Add example config to storybook's preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,25 @@
-import type { Preview } from '@storybook/react-vite';
+import type { Preview, StoryContext, StoryFn } from '@storybook/react-vite';
+import { renderToStaticMarkup } from 'react-dom/server';
+import type { Renderer, PartialStoryFn as StoryFunction } from 'storybook/internal/types';
 
 const preview: Preview = {
   parameters: {
     docsHtml: {
-      panelTitle: 'CUSTOM HTML',
-      canvasToggleText: {
-        opened: 'CUSTOM Hide HTML',
-        closed: 'CUSTOM Show HTML',
-      },
+      // panelTitle: 'HTML',
+      // canvasToggleText: {
+      //   opened: 'Hide HTML',
+      //   closed: 'Show HTML',
+      // },
+      // root: '#my-custom-container',
       // theme: 'shades-of-purple',
+      // disable: false,
+      // retrieveHtml: (storyFn: StoryFunction<Renderer>, context: StoryContext) => {
+      //   const renderedHtml = renderToStaticMarkup(storyFn(context));
+      //   return renderedHtml;
+      // },
+      // transform: function (code: string) {
+      //   return code;
+      // },
     },
     controls: {
       matchers: {
