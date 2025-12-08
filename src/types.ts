612823d7ea839994dd8bf9e5c29c1b4903683664
Dan White <dan.white1@wsu.edu>
2025-12-08 13:29:20 -0800

    Updates parameters for configuration
    Adds a `disable` option to the parameters, allowing the disabling of the addon.
    Also reorders the `theme` and `disable` fields in the `Parameters` interface for better organization and removes unused `Result` interface.

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,11 @@
 import { type StoryContext, type StoryFn } from '@storybook/react-vite';
 
 export interface Parameters {
-  theme?: string;
   root?: string;
   panelTitle?: string;
   canvasToggleText?: { opened: string; closed: string };
+  theme?: string;
+  disable?: boolean;
   retrieveHtml?: (StoryFn: StoryFn, context: StoryContext) => string | Promise<string>;
   transform?: (code: string) => string | Promise<string>;
 }
@@ -13,10 +14,4 @@ export interface CodeUpdateData {
   id: string;
   html: string;
   params: Parameters;
-}
-
-// TODO: Delete
-export interface Result {
-  divs: DOMRect[];
-  styled: DOMRect[];
 }
